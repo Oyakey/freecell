53a15743c927186ec4c1289fcaad62bd630aec48
Julien <julien@macbookair.home>
2025-07-25 23:25:22 +0200

    wip: victory screen

---
+++ VictoryScreen.cs
@@ -0,0 +1,23 @@
+﻿using Godot;
+
+public partial class VictoryScreen : Control
+{
+    private void _ready()
+    {
+        Visible = false;
+    }
+
+    private void _process(float delta)
+    {
+        // TODO: Add winning game logic here.
+        // We can check the foundations every frame and see if they're all filled, but that's expensive.
+        // Or we can expose a method that foundations call when they were filled to ehance performance.
+    }
+
+    private void Win()
+    {
+        // For now, just pause the game and show the victory screen.
+        Visible = true;
+        GetTree().Paused = true;
+    }
+}
